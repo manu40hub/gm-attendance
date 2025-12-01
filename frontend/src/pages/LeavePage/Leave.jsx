cf6bf32886827fb3b03741a3ee3753af9fb6bcfb
Gonugunta Manohar Kumar <gonuguntamanoharkumar@gmail.com>
2025-12-01 23:17:50 +0530

    Fix import path for apiClient in Leave.jsx

--- frontend/src/pages/LeavePage/Leave.jsx
+++ frontend/src/pages/LeavePage/Leave.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import api from "../apiClient";
+import api from "../../apiClient";
 import "./Leave.css";
 import { useNavigate } from "react-router-dom";
 import moment from "moment";
