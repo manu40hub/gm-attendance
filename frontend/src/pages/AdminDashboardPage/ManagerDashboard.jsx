23e19cc55bc9fdab51b87838edb99b565195a754
Gonugunta Manohar Kumar <gonuguntamanoharkumar@gmail.com>
2025-12-01 23:17:02 +0530

    Fix import path for apiClient in ManagerDashboard

--- frontend/src/pages/AdminDashboardPage/ManagerDashboard.jsx
+++ frontend/src/pages/AdminDashboardPage/ManagerDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import api from "../apiClient";
+import api from "../../apiClient";
 import moment from "moment";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
